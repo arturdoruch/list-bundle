315f24338ece823f9221f7b8bcd698cc73a6c927
Artur Doruch <arturdoruch@interia.pl>
2021-03-28 18:29:20 +0200

    DependencyInjection\ArturDoruchListExtension: Optimize imported namespaces

--- DependencyInjection/ArturDoruchListExtension.php
+++ DependencyInjection/ArturDoruchListExtension.php
@@ -2,10 +2,10 @@
 
 namespace ArturDoruch\ListBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class ArturDoruchListExtension extends Extension
 {
@@ -14,7 +14,7 @@ class ArturDoruchListExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
         $container->setParameter('arturdoruch_list.query_parameter_names', $config['query_parameter_names']);
